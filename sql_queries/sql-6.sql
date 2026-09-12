f829747125256a201b92bedccdf84d2edd3d8464
NMahmood65 <97833031+NMahmood65@users.noreply.github.com>
2026-09-12 18:56:15 +0100

    Delete sql_queries/sql-6.sql

--- sql_queries/sql-6.sql
+++
@@ -1,6 +0,0 @@
-use supply_chain_project;
--- UPDATE sales 
--- SET Description = 'Description Unavailable' 
--- WHERE Description LIKE '?%' OR Description = '';
-
-SELECT DISTINCT Description FROM sales ORDER BY Description ASC LIMIT 20;
