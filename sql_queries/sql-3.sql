477e615a85388313db0e647083e69dd1cd10c7b9
NMahmood65 <97833031+NMahmood65@users.noreply.github.com>
2026-09-12 18:55:44 +0100

    Delete sql_queries/sql-3.sql

--- sql_queries/sql-3.sql
+++
@@ -1,8 +0,0 @@
-use supply_chain_project;
-
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 26.7/Uploads/sales.csv'
-INTO TABLE sales
-FIELDS TERMINATED BY ',' 
-OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 LINES;
